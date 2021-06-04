34d21896582b95e2b4cc361685e9e69921891351
united36 <59273001+united36@users.noreply.github.com>
2021-06-04 15:22:01 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@ git clone https://github.com/projectdiscovery/httpx.git; cd httpx/cmd/httpx; go 
 
 git clone https://github.com/devanshbatham/ParamSpider
 
+git clone https://github.com/danielmiessler/SecLists
+
 git clone https://github.com/projectdiscovery/nuclei.git; cd nuclei/v2/cmd/nuclei/; go build; mv nuclei /usr/local/bin/; nuclei -version
 
 nuclei -update-templates
